a2d984a91700f300246d02ac91b73cc70a29f64c
lkbrian <lkbrian742@gmail.com>
2023-12-19 09:03:21 +0300

    completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(record => record.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }
